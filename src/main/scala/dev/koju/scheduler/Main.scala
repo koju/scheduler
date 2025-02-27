ff91eb74ebc314cecdfed793f5105f2d05c7d2f9
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2025-02-26 23:18:21 -0500

    Make LoggerFactory based on F instead of IO

--- src/main/scala/dev/koju/scheduler/Main.scala
+++ src/main/scala/dev/koju/scheduler/Main.scala
@@ -1,11 +1,6 @@
 package dev.koju.scheduler
 
 import cats.effect.{IO, IOApp}
-import org.typelevel.log4cats.LoggerFactory
-import org.typelevel.log4cats.slf4j.Slf4jFactory
 
 object Main extends IOApp.Simple:
-  override val run: IO[Unit] =
-    given LoggerFactory[IO] = Slf4jFactory.create[IO]
-
-    SchedulerServer.run[IO]
+  override val run: IO[Unit] = SchedulerServer.run[IO]
--- src/main/scala/dev/koju/scheduler/SchedulerServer.scala
+++ src/main/scala/dev/koju/scheduler/SchedulerServer.scala
@@ -9,10 +9,13 @@ import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.implicits.*
 import org.http4s.server.middleware.Logger
 import org.typelevel.log4cats.*
+import org.typelevel.log4cats.slf4j.Slf4jFactory
 
 object SchedulerServer:
 
-  def run[F[_] : {Async, Network, LoggerFactory}]: F[Nothing] = {
+  def run[F[_] : {Async, Network}]: F[Nothing] = {
+    given LoggerFactory[F] = Slf4jFactory.create[F]
+
     for {
       client <- EmberClientBuilder.default[F].build
       helloWorldAlg = HelloWorld.impl[F]
