b05bf5f9949b9603525905c5fd09cdff8e799dd4
Kapil Koju <960364+TheKojuEffect@users.noreply.github.com>
2025-03-14 23:29:10 -0400

    Set up to auto merge scala-steward PRs

--- src/main/scala/dev/koju/scheduler/Main.scala
+++ src/main/scala/dev/koju/scheduler/Main.scala
@@ -3,4 +3,4 @@ package dev.koju.scheduler
 import cats.effect.{IO, IOApp}
 
 object Main extends IOApp.Simple:
-  override val run: IO[Unit] = SchedulerServer.run[IO]
+  val run = SchedulerServer.run[IO]
